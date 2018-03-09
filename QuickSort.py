909193891c012d70cad9ef08db98fce499b59b3e
liuwei <liuwei@polex.com.cn>
2018-03-09 09:10:55 +0800

    add 第四章快速排序
    增加 第四章快速排序代码

---
+++ QuickSort.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+
+def quicksort(array):
+    """快速排序算法"""
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+if __name__ == '__main__':
+    print(quicksort([10, 5, 2, 3]))
